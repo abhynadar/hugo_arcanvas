2312668b172319d0d88ee7f5ed41dc998d4e2038
Abhy Nadar <abhy.nadar@3pillar.corp>
2017-03-30 10:46:50 +0530

    changed layouts and added new posts

---
+++ newpost.bat
@@ -0,0 +1,11 @@
+echo off
+set title=%1
+echo %title%
+echo "creating content file..."
+hugo new %title%.md
+echo "marking content as draft=false..."
+hugo undraft %title%.md
+echo "opening content file in sublime..."
+subl content/%title%.md
+echo "done..."
+
---
+++ publish.bat
@@ -0,0 +1,16 @@
+echo off
+set comment=%1
+echo %comment%
+echo 'initiating site generation...'
+hugo --theme=hugo_theme_robust
+echo 'site generated in public folder...'
+cd public
+echo 'moved to public folder...'
+echo 'adding files...'
+git add --all
+echo 'commiting changes...'
+git commit -m%comment%
+echo 'pushing to repo...'
+git push -f origin gh-pages
+echo 'finished publishing site!!!'
+cd..
